4d6938f33ea9a6fc168e0662f0c473ea87cd83d5
KkolyadkoO <artem.7ko52@gmail.com>
2023-09-20 09:22:10 +0300

    Clear trash

--- PPOIS/Lab1/build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/user/Desktop/УНИК/BSUIR/PPOIS/Lab1/test
-# Build directory: /Users/user/Desktop/УНИК/BSUIR/PPOIS/Lab1/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/user/Desktop/УНИК/BSUIR/PPOIS/Lab1/build/test/Cube_Test[1]_include.cmake")
-subdirs("../_deps/googletest-build")
--- PPOIS/Lab1/build/test/Cube_Test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/user/Desktop/УНИК/BSUIR/PPOIS/Lab1/build/test/Cube_Test[1]_tests.cmake")
-  include("/Users/user/Desktop/УНИК/BSUIR/PPOIS/Lab1/build/test/Cube_Test[1]_tests.cmake")
-else()
-  add_test(Cube_Test_NOT_BUILT Cube_Test_NOT_BUILT)
-endif()
