d104ad76a396eacfb798becf47a720b09342448d
Kolyadko <artem.7ko52@gmail.com>
2023-10-06 13:04:50 +0300

    metra lab2

--- MCiCvIT/Lab1/metr1.cpp
+++ MCiCvIT/Lab1/metr1.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 int main()
 {
-	int n;//1
-	cin >> n; //1
-	if (n>100 || n<0)//1
+	
+	int n;//1s
+	cin >> n; //1s
+
+	if (n>100 || n<0)//1s
 	{
 		cout << "Unkorrekt N";
 		return 0;
@@ -14,7 +16,7 @@ int main()
 	for (int i = 0; i < n; i++) //n
 	{
 		cin >> Arr[i];//n
-		if (Arr[i] > 0)//n
+		if (Arr[i] > 0)//na
 			Arr[i] = pow(Arr[i], 3);//n
 		if (Arr[i] < 0)//n
 			Arr[i] = pow(Arr[i], 2);
@@ -26,5 +28,5 @@ int main()
 	return 0;//1
 }
 
-// Память максимально n+1=101
-// время максимально 1+1+1+1+5*n+2*n+1=705с
+// Память максимально n+1=101 O(n+1)=101
+// время максимально 1+1+1+1+5*n+2*n+1=705с O(n)
