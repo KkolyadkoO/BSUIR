891484ba4e916270a072bfc7d03ac3b441bd6dfe
Kolyadko <artem.7ko52@gmail.com>
2023-10-10 09:31:29 +0300

    ppois lab 2 fix

--- PPOIS/Lab2/src/postal_office.cpp
+++ PPOIS/Lab2/src/postal_office.cpp
@@ -1,109 +1,124 @@
 #include "../inc/postal_office.h"
 
 // Postal Office ---------------------------------------------------
-PostalOffice::PostalOffice(const std::string& address_, const std::string& openingHours_, const std::string& service_)
-: address(address_), openingHours(openingHours_), service(service_) {}
+PostalOffice::PostalOffice(const std::string &address_, const std::string &openingHours_, const std::string &service_)
+    : address(address_), openingHours(openingHours_), service(service_) {}
 
-void PostalOffice::displayInfo() const{
-    std::cout << "Postal Office Information:"<<std::endl;
+void PostalOffice::displayInfo() const
+{
+    std::cout << "Postal Office Information:" << std::endl;
     std::cout << "Address: " << PostalOffice::address << std::endl;
     std::cout << "Opening hours: " << PostalOffice::openingHours << std::endl;
     std::cout << "Service: " << PostalOffice::service << std::endl;
 }
 
-std::string PostalOffice::getAddress() const{
+std::string PostalOffice::getAddress() const
+{
     return address;
 }
-std::string PostalOffice::getOpeningHours() const{
+std::string PostalOffice::getOpeningHours() const
+{
     return openingHours;
 }
-std::string PostalOffice::getService() const{
+std::string PostalOffice::getService() const
+{
     return service;
 }
-void PostalOffice::loadClients(std::ifstream& fileDate){
-        std::string name, address, phoneNumber, mail, postcode, clientID;
-        std::getline(fileDate, name);
-        getline(fileDate, address);
-        getline(fileDate, phoneNumber);
-        getline(fileDate, mail);
-        getline(fileDate, clientID);
-        getline(fileDate, postcode);
-        Customer customer(name,address,phoneNumber,mail,stoi(clientID),postcode);
-        clients.push_back(customer);
+void PostalOffice::loadClients(std::ifstream &fileDate)
+{
+    std::string name, address, phoneNumber, mail, postcode, clientID;
+    std::getline(fileDate, name);
+    getline(fileDate, address);
+    getline(fileDate, phoneNumber);
+    getline(fileDate, mail);
+    getline(fileDate, clientID);
+    getline(fileDate, postcode);
+    Customer customer(name, address, phoneNumber, mail, stoi(clientID), postcode);
+    clients.push_back(customer);
 }
-void PostalOffice::showClients(){
+void PostalOffice::showClients()
+{
     for (size_t i = 0; i < clients.size(); i++)
     {
-        std::cout << "---------------------------- "<< "Client number " << i+1 << " ----------------------------"<<std::endl;
+        std::cout << "---------------------------- "
+                  << "Client number " << i + 1 << " ----------------------------" << std::endl;
         clients[i].displayInfo();
     }
-    
 }
-void PostalOffice::loadPostmans(std::ifstream& fileDate){
-    
-        std::string name, address, phoneNumber, mail, salary,position;
-        std::getline(fileDate, name);
-        getline(fileDate, address);
-        getline(fileDate, phoneNumber);
-        getline(fileDate, mail);
-        getline(fileDate, salary);
-        getline(fileDate, position);
-        if(position == "sorter"){
-            Sorter sorter(name, address,phoneNumber,mail,stoi(salary));
-            sorters.push_back(sorter);
-        }
-        if(position == "deliveryman"){
-            Deliveryman deliveryman(name, address,phoneNumber,mail,stoi(salary));
-            deliverymans.push_back(deliveryman);
-        }
-            
+void PostalOffice::loadPostmans(std::ifstream &fileDate)
+{
+
+    std::string name, address, phoneNumber, mail, salary, position;
+    std::getline(fileDate, name);
+    getline(fileDate, address);
+    getline(fileDate, phoneNumber);
+    getline(fileDate, mail);
+    getline(fileDate, salary);
+    getline(fileDate, position);
+    if (position == "sorter")
+    {
+        Sorter sorter(name, address, phoneNumber, mail, stoi(salary));
+        sorters.push_back(sorter);
+    }
+    if (position == "deliveryman")
+    {
+        Deliveryman deliveryman(name, address, phoneNumber, mail, stoi(salary));
+        deliverymans.push_back(deliveryman);
+    }
 }
-void PostalOffice::showPostmans(){
+void PostalOffice::showPostmans()
+{
     for (size_t i = 0; i < sorters.size(); i++)
     {
-        std::cout << "---------------------------- "<< "Sorter number " << i+1 << " ----------------------------"<<std::endl;
+        std::cout << "---------------------------- "
+                  << "Sorter number " << i + 1 << " ----------------------------" << std::endl;
         sorters[i].displayInfo();
     }
     for (size_t i = 0; i < deliverymans.size(); i++)
     {
-        std::cout << "---------------------------- "<< "Deliveryman number " << i+1 << " ----------------------------"<<std::endl;
+        std::cout << "---------------------------- "
+                  << "Deliveryman number " << i + 1 << " ----------------------------" << std::endl;
         deliverymans[i].displayInfo();
     }
 }
-void PostalOffice::loadLetter(std::ifstream& fileDate){
-        bool senderFinded=false,recipientFinded=false;
-        int senderNumber,recipientNumber;
-        std::string weight, location, text="", senderID, recipientID,allText = "";
-        getline(fileDate, senderID);
-        getline(fileDate, recipientID);
-        getline(fileDate, weight);
-        getline(fileDate, location);
-        while(text!="END TEXT"){
+void PostalOffice::loadLetter(std::ifstream &fileDate)
+{
+    bool senderFinded = false, recipientFinded = false;
+    int senderNumber, recipientNumber;
+    std::string weight, location, text = "", senderID, recipientID, allText = "";
+    getline(fileDate, senderID);
+    getline(fileDate, recipientID);
+    getline(fileDate, weight);
+    getline(fileDate, location);
+    while (text != "END TEXT")
+    {
         getline(fileDate, text);
-        allText+= text;
-        }
-        for (size_t i = 0; i < clients.size(); i++)
+        allText += text;
+    }
+    for (size_t i = 0; i < clients.size(); i++)
+    {
+        if (stoi(senderID) == clients[i].getClientID())
         {
-            if(stoi(senderID)==clients[i].getClientID())
-            {
-                senderNumber = i;
-                senderFinded = true;
-            }
-            if(stoi(recipientID)==clients[i].getClientID())
-            {
-                recipientNumber = i;
-                recipientFinded = true;
-            }
+            senderNumber = i;
+            senderFinded = true;
         }
-        if(recipientFinded && senderFinded){
-            Letter letter(clients[senderNumber],clients[recipientNumber],stof(weight),location,allText);
-            letters.push_back(letter);
+        if (stoi(recipientID) == clients[i].getClientID())
+        {
+            recipientNumber = i;
+            recipientFinded = true;
         }
+    }
+    if (recipientFinded && senderFinded)
+    {
+        Letter letter(clients[senderNumber], clients[recipientNumber], stof(weight), location, allText);
+        letters.push_back(letter);
+    }
 }
-void PostalOffice::loadParcel(std::ifstream& fileDate){
-    bool senderFinded=false,recipientFinded=false;
-    int senderNumber,recipientNumber;
-    std::string weight, location,senderID,recipientID,length,width,height;
+void PostalOffice::loadParcel(std::ifstream &fileDate)
+{
+    bool senderFinded = false, recipientFinded = false;
+    int senderNumber, recipientNumber;
+    std::string weight, location, senderID, recipientID, length, width, height;
     getline(fileDate, senderID);
     getline(fileDate, recipientID);
     getline(fileDate, weight);
@@ -114,32 +129,35 @@ void PostalOffice::loadParcel(std::ifstream& fileDate){
 
     for (size_t i = 0; i < clients.size(); i++)
     {
-        if(stoi(senderID)==clients[i].getClientID())
-            {
-                senderNumber = i;
-                senderFinded = true;
-            }
-        if(stoi(recipientID)==clients[i].getClientID())
-            {
-                recipientNumber = i;
-                recipientFinded = true;
-            }
+        if (stoi(senderID) == clients[i].getClientID())
+        {
+            senderNumber = i;
+            senderFinded = true;
+        }
+        if (stoi(recipientID) == clients[i].getClientID())
+        {
+            recipientNumber = i;
+            recipientFinded = true;
+        }
     }
-    if(recipientFinded && senderFinded){
-        Parcel parcel(clients[senderNumber],clients[recipientNumber],stof(weight),location,stof(length),stof(width),stof(height));
+    if (recipientFinded && senderFinded)
+    {
+        Parcel parcel(clients[senderNumber], clients[recipientNumber], stof(weight), location, stof(length), stof(width), stof(height));
         parcels.push_back(parcel);
     }
 }
 
-void PostalOffice::sortPackage(){
-    if(sorters.size()==0){
-    std::cout << "There are no srters at the post office"; 
-    return;
+void PostalOffice::sortPackage()
+{
+    if (sorters.size() == 0)
+    {
+        std::cout << "There are no srters at the post office";
+        return;
     }
-    sorters[sorters.size()-1].sortPackage(letters,parcels,address);
+    sorters[sorters.size() - 1].sortPackage(letters, parcels, address);
     for (size_t i = 0; i < letters.size(); i++)
     {
-        if(letters[i].getOnPostOffice())
+        if (letters[i].getOnPostOffice())
         {
             for (size_t j = 0; j < deliverymans.size(); j++)
             {
@@ -149,7 +167,7 @@ void PostalOffice::sortPackage(){
     }
     for (size_t i = 0; i < parcels.size(); i++)
     {
-        if(parcels[i].getOnPostOffice())
+        if (parcels[i].getOnPostOffice())
         {
             for (size_t j = 0; j < deliverymans.size(); j++)
             {
@@ -158,162 +176,192 @@ void PostalOffice::sortPackage(){
         }
     }
 }
-void PostalOffice::loadDate(std::string filename){
+void PostalOffice::loadDate(std::string filename)
+{
     std::ifstream fileDate(filename);
-    if (fileDate.is_open()) {
-        bool senderFinded=false,recipientFinded=false;
-        int senderNumber,recipientNumber;
+    if (fileDate.is_open())
+    {
+        bool senderFinded = false, recipientFinded = false;
+        int senderNumber, recipientNumber;
         std::string typeOfLoading;
-        while (std::getline(fileDate, typeOfLoading)) {
-            if(typeOfLoading == "Letter"){
+        while (std::getline(fileDate, typeOfLoading))
+        {
+            if (typeOfLoading == "Letter")
+            {
                 loadLetter(fileDate);
-           }
-            if(typeOfLoading == "Postman"){
+            }
+            if (typeOfLoading == "Postman")
+            {
                 loadPostmans(fileDate);
-           }
-            if(typeOfLoading == "Parcel"){
+            }
+            if (typeOfLoading == "Parcel")
+            {
                 loadParcel(fileDate);
             }
-            if(typeOfLoading == "Client"){
+            if (typeOfLoading == "Client")
+            {
                 loadClients(fileDate);
             }
         }
 
         fileDate.close();
-    } else {
+    }
+    else
+    {
         std::cout << "Unable to open file." << std::endl;
     }
 }
-int PostalOffice::getNumberOfClients(){
+int PostalOffice::getNumberOfClients()
+{
     return clients.size();
 }
-int PostalOffice::getNumberOfPostmans(){
-    return sorters.size()+deliverymans.size();
+int PostalOffice::getNumberOfPostmans()
+{
+    return sorters.size() + deliverymans.size();
 }
-int PostalOffice::getPackageInStock(){
+int PostalOffice::getPackageInStock()
+{
     return deliverymans[0].getPackageInStock();
 }
 
-
-
 // Person ---------------------------------------------------
-Person::Person(const std::string& name_,const std::string& address_,const std::string& phoneNumber_,const std::string& mail_)
-: name(name_), address(address_),phoneNumber(phoneNumber_),mail(mail_){}
+Person::Person(const std::string &name_, const std::string &address_, const std::string &phoneNumber_, const std::string &mail_)
+    : name(name_), address(address_), phoneNumber(phoneNumber_), mail(mail_) {}
 
-void Person::displayInfo() const{
+void Person::displayInfo() const
+{
     std::cout << "Name: " << Person::name << std::endl;
     std::cout << "Address: " << Person::address << std::endl;
     std::cout << "Phone number: " << Person::phoneNumber << std::endl;
     std::cout << "Mail: " << Person::mail << std::endl;
 }
 
-std::string Person::getAddress() const{
+std::string Person::getAddress() const
+{
     return address;
 }
-std::string Person::getName() const{
+std::string Person::getName() const
+{
     return name;
 }
-std::string Person::getPhoneNumer() const{
+std::string Person::getPhoneNumer() const
+{
     return phoneNumber;
 }
-std::string Person::getMail() const{
+std::string Person::getMail() const
+{
     return mail;
 }
 
 // Customer ---------------------------------------------------
-Customer::Customer(const std::string& name_,const std::string& address_,const std::string& phoneNumber_,const std::string& mail_, int clientID_,const std::string postcode_)
-:Person(name_,address_,phoneNumber_,mail_),clientID(clientID_),postcode(postcode_){}
+Customer::Customer(const std::string &name_, const std::string &address_, const std::string &phoneNumber_, const std::string &mail_, int clientID_, const std::string postcode_)
+    : Person(name_, address_, phoneNumber_, mail_), clientID(clientID_), postcode(postcode_) {}
 
-void Customer::displayInfo()const{
-    std::cout << "Customer Information:"<<std::endl;
+void Customer::displayInfo() const
+{
+    std::cout << "Customer Information:" << std::endl;
     Person::displayInfo();
     std::cout << "ClientID: " << clientID << std::endl;
     std::cout << "Postcode: " << postcode << std::endl;
 }
-int Customer::getClientID(){
+int Customer::getClientID()
+{
     return clientID;
 }
-std::string Customer::getPostcode(){
+std::string Customer::getPostcode()
+{
     return postcode;
 }
 
 // PostMan ---------------------------------------------------
-PostMan::PostMan(const std::string& name_,const std::string& address_,const std::string& phoneNumber_,const std::string& mail_, int salary_)
-:Person(name_,address_,phoneNumber_,mail_),salary(salary_){}
-void PostMan::displayInfo()const{
-    std::cout << "Postman Information:"<<std::endl;
+PostMan::PostMan(const std::string &name_, const std::string &address_, const std::string &phoneNumber_, const std::string &mail_, int salary_)
+    : Person(name_, address_, phoneNumber_, mail_), salary(salary_) {}
+void PostMan::displayInfo() const
+{
+    std::cout << "Postman Information:" << std::endl;
     Person::displayInfo();
     std::cout << "Salary: " << salary << std::endl;
 }
-int PostMan::getSalary(){
+int PostMan::getSalary()
+{
     return salary;
 }
 
 // Package ---------------------------------------------------
-Package::Package(Customer sender_,Customer recipient_,double weight_,const std::string& location_)
-:location(location_),sender(sender_),recipient(recipient_),weight(weight_){
-    address.setDeliveryAddress(recipient_.getPostcode(),recipient_.getAddress());
+Package::Package(Customer sender_, Customer recipient_, double weight_, const std::string &location_)
+    : location(location_), sender(sender_), recipient(recipient_), weight(weight_)
+{
+    address.setDeliveryAddress(recipient_.getPostcode(), recipient_.getAddress());
 }
-std::string Package::getLocation(){
+std::string Package::getLocation()
+{
     return location;
 }
-void Package::setLocation(const std::string& location_){
+void Package::setLocation(const std::string &location_)
+{
     location = location_;
 }
-void Package::setOnPostOffice(bool value){
+void Package::setOnPostOffice(bool value)
+{
     onPostOffice = value;
 }
-bool Package::getOnPostOffice(){
+bool Package::getOnPostOffice()
+{
     return onPostOffice;
 }
 
-
 // Letter ---------------------------------------------------
-Letter::Letter(Customer sender_,Customer recipient_,double weight_,const std::string& location_,const std::string& letterText_)
-:Package(sender_,recipient_,weight_,location_),letterText(letterText_){}
+Letter::Letter(Customer sender_, Customer recipient_, double weight_, const std::string &location_, const std::string &letterText_)
+    : Package(sender_, recipient_, weight_, location_), letterText(letterText_) {}
 
 // Parcel ---------------------------------------------------
-Parcel::Parcel(Customer sender_,Customer recipient_,double weight_,const std::string& location_, double length_, double width_, double height_)
-:Package(sender_,recipient_,weight_,location_),length(length_),width(width_),height(height_){}
+Parcel::Parcel(Customer sender_, Customer recipient_, double weight_, const std::string &location_, double length_, double width_, double height_)
+    : Package(sender_, recipient_, weight_, location_), length(length_), width(width_), height(height_) {}
 
 // DeliveryAddress ---------------------------------------------------
-void DeliveryAddress::setDeliveryAddress(const std::string& postcode_,const std::string& address_)
+void DeliveryAddress::setDeliveryAddress(const std::string &postcode_, const std::string &address_)
 {
     postcode = postcode_;
     address = address_;
 }
-std::string DeliveryAddress::getAddress(){
+std::string DeliveryAddress::getAddress()
+{
     return address;
 }
-std::string DeliveryAddress::getPostcode(){
+std::string DeliveryAddress::getPostcode()
+{
     return postcode;
 }
 
 // Sorter --------------------------------------------------
-Sorter::Sorter(const std::string& name_,const std::string& address_,const std::string& phoneNumber_,const std::string& mail_, int salary_)
-:PostMan(name_,address_,phoneNumber_,mail_,salary_){}
-void Sorter::sortPackage(std::vector<Letter>& letters,std::vector<Parcel>& parcels,std::string postOfficeAddress){
+Sorter::Sorter(const std::string &name_, const std::string &address_, const std::string &phoneNumber_, const std::string &mail_, int salary_)
+    : PostMan(name_, address_, phoneNumber_, mail_, salary_) {}
+void Sorter::sortPackage(std::vector<Letter> &letters, std::vector<Parcel> &parcels, std::string postOfficeAddress)
+{
     for (size_t i = 0; i < letters.size(); i++)
     {
-        if(letters[i].getLocation()==postOfficeAddress)
-        letters[i].setOnPostOffice(true);
+        if (letters[i].getLocation() == postOfficeAddress)
+            letters[i].setOnPostOffice(true);
     }
     for (size_t i = 0; i < parcels.size(); i++)
     {
-        if(parcels[i].getLocation()==postOfficeAddress)
-        parcels[i].setOnPostOffice(true);
+        if (parcels[i].getLocation() == postOfficeAddress)
+            parcels[i].setOnPostOffice(true);
     }
 }
 
 // Deliveryman ---------------------------------------------
-Deliveryman::Deliveryman(const std::string& name_,const std::string& address_,const std::string& phoneNumber_,const std::string& mail_, int salary_)
-:PostMan(name_,address_,phoneNumber_,mail_,salary_){}
-void Deliveryman::addLetter(Letter letter){
+Deliveryman::Deliveryman(const std::string &name_, const std::string &address_, const std::string &phoneNumber_, const std::string &mail_, int salary_)
+    : PostMan(name_, address_, phoneNumber_, mail_, salary_) {}
+void Deliveryman::addLetter(Letter letter)
+{
     lettersInStock.push_back(letter);
 }
-void Deliveryman::addParcel(Parcel parcel){
+void Deliveryman::addParcel(Parcel parcel)
+{
     parcelInStock.push_back(parcel);
 }
-int Deliveryman::getPackageInStock(){
-    return lettersInStock.size()+parcelInStock.size();
+int Deliveryman::getPackageInStock()
+{
+    return lettersInStock.size() + parcelInStock.size();
 }
--- PPOIS/Lab2/test/test.cpp
+++ PPOIS/Lab2/test/test.cpp
@@ -2,42 +2,47 @@
 #include <sstream>
 #include "../inc/postal_office.h"
 
-TEST(POST_Test, PostalOfficeConstruct) {
-  PostalOffice office("address","9 am - 10 pm","mail");
-  EXPECT_TRUE(office.getAddress()=="address");
-  EXPECT_TRUE(office.getOpeningHours()=="9 am - 10 pm");
-  EXPECT_TRUE(office.getService()=="mail");
+TEST(POST_Test, PostalOfficeConstruct)
+{
+  PostalOffice office("address", "9 am - 10 pm", "mail");
+  EXPECT_TRUE(office.getAddress() == "address");
+  EXPECT_TRUE(office.getOpeningHours() == "9 am - 10 pm");
+  EXPECT_TRUE(office.getService() == "mail");
 }
 
-TEST(POST_Test, ClientFun) {
-  Customer customer("Name","address","375xxxx","mail@mail.com",1,"220116");
-  EXPECT_TRUE(customer.getName()=="Name");
-  EXPECT_TRUE(customer.getAddress()=="address");
-  EXPECT_TRUE(customer.getPhoneNumer()=="375xxxx");
-  EXPECT_TRUE(customer.getMail()=="mail@mail.com");
-  EXPECT_TRUE(customer.getPostcode()=="220116");
-  EXPECT_EQ(customer.getClientID(),1);
+TEST(POST_Test, ClientFun)
+{
+  Customer customer("Name", "address", "375xxxx", "mail@mail.com", 1, "220116");
+  EXPECT_TRUE(customer.getName() == "Name");
+  EXPECT_TRUE(customer.getAddress() == "address");
+  EXPECT_TRUE(customer.getPhoneNumer() == "375xxxx");
+  EXPECT_TRUE(customer.getMail() == "mail@mail.com");
+  EXPECT_TRUE(customer.getPostcode() == "220116");
+  EXPECT_EQ(customer.getClientID(), 1);
 }
-TEST(POST_Test, PostmanFun) {
-  Sorter sorter("Name","address","375xxxx","mail@mail.com",1000);
-  EXPECT_TRUE(sorter.getName()=="Name");
-  EXPECT_TRUE(sorter.getAddress()=="address");
-  EXPECT_TRUE(sorter.getPhoneNumer()=="375xxxx");
-  EXPECT_TRUE(sorter.getMail()=="mail@mail.com");
-  EXPECT_EQ(sorter.getSalary(),1000);
+TEST(POST_Test, PostmanFun)
+{
+  Sorter sorter("Name", "address", "375xxxx", "mail@mail.com", 1000);
+  EXPECT_TRUE(sorter.getName() == "Name");
+  EXPECT_TRUE(sorter.getAddress() == "address");
+  EXPECT_TRUE(sorter.getPhoneNumer() == "375xxxx");
+  EXPECT_TRUE(sorter.getMail() == "mail@mail.com");
+  EXPECT_EQ(sorter.getSalary(), 1000);
 }
 
-TEST(POST_Test, DeliveryAddressFun) {
+TEST(POST_Test, DeliveryAddressFun)
+{
   DeliveryAddress address;
-  address.setDeliveryAddress("220116","Address");
-  EXPECT_TRUE(address.getAddress()=="Address");
-  EXPECT_TRUE(address.getPostcode()=="220116");
+  address.setDeliveryAddress("220116", "Address");
+  EXPECT_TRUE(address.getAddress() == "Address");
+  EXPECT_TRUE(address.getPostcode() == "220116");
 }
 
-TEST(POST_Test, LetterFun) {
-  Customer sender("Name","address","375xxxx","mail@mail.com",1,"220116");
-  Customer recipient("Name","address","375xxxx","mail@mail.com",2,"220116");
-  Letter letter(sender,recipient,100.2,"China","Text");
+TEST(POST_Test, LetterFun)
+{
+  Customer sender("Name", "address", "375xxxx", "mail@mail.com", 1, "220116");
+  Customer recipient("Name", "address", "375xxxx", "mail@mail.com", 2, "220116");
+  Letter letter(sender, recipient, 100.2, "China", "Text");
   EXPECT_TRUE(letter.getLocation() == "China");
   letter.setLocation("Belarus");
   EXPECT_TRUE(letter.getLocation() == "Belarus");
@@ -45,10 +50,11 @@ TEST(POST_Test, LetterFun) {
   EXPECT_FALSE(letter.getOnPostOffice());
 }
 
-TEST(POST_Test, ParcelFun) {
-  Customer sender("Name","address","375xxxx","mail@mail.com",1,"220116");
-  Customer recipient("Name","address","375xxxx","mail@mail.com",2,"220116");
-  Parcel parcel(sender, recipient,100.2,"China", 10.2,13,45);
+TEST(POST_Test, ParcelFun)
+{
+  Customer sender("Name", "address", "375xxxx", "mail@mail.com", 1, "220116");
+  Customer recipient("Name", "address", "375xxxx", "mail@mail.com", 2, "220116");
+  Parcel parcel(sender, recipient, 100.2, "China", 10.2, 13, 45);
   EXPECT_DOUBLE_EQ(parcel.weight, 100.2);
   EXPECT_TRUE(parcel.getLocation() == "China");
   parcel.setLocation("Belarus");
@@ -57,11 +63,12 @@ TEST(POST_Test, ParcelFun) {
   EXPECT_FALSE(parcel.getOnPostOffice());
 }
 
-TEST(POST_Test, functional) {
-  PostalOffice office("Belarus","9 am - 10 pm","mail");
+TEST(POST_Test, functional)
+{
+  PostalOffice office("Belarus", "9 am - 10 pm", "mail");
   office.loadDate("date.txt");
-  EXPECT_EQ(office.getNumberOfClients(),2);
-  EXPECT_EQ(office.getNumberOfPostmans(),2);
+  EXPECT_EQ(office.getNumberOfClients(), 2);
+  EXPECT_EQ(office.getNumberOfPostmans(), 2);
   office.sortPackage();
-  EXPECT_EQ(office.getPackageInStock(),1);
+  EXPECT_EQ(office.getPackageInStock(), 1);
 }
